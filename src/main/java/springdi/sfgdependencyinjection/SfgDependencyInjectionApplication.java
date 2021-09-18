ae23d9e834d9b3deb54b4ad7f477d804cee1a172
sindhusreejas <sindhusreejas@gmail.com>
2021-09-18 12:54:17 +0530

    Initial commit

---
+++ src/main/java/springdi/sfgdependencyinjection/SfgDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package springdi.sfgdependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDependencyInjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springdi/sfgdependencyinjection/SfgDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package springdi.sfgdependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
